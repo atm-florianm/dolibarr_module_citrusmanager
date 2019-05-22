0d70da57cd8741db0adf7000fccf2e4f147a5e9c
atm-florian.m <florian.mortgat@atm-consulting.fr>
2019-05-22 12:28:55 +0200

    Added a basic hook (as an example).

--- core/modules/modCitrusmanager.class.php
+++ core/modules/modCitrusmanager.class.php
@@ -92,7 +92,7 @@ class modCitrusmanager extends DolibarrModules
 			'models' => 0,                                   	// Set this to 1 if module has its own models directory (core/modules/xxx)
 			'css' => array('/citrusmanager/css/citrusmanager.css.php'),	// Set this to relative path of css file if module has its own css file
 	 		'js' => array('/citrusmanager/js/citrusmanager.js.php'),          // Set this to relative path of js file if module must load a js on all pages
-			'hooks' => array('data'=>array('hookcontext1','hookcontext2'), 'entity'=>'0'), 	// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context 'all'
+			'hooks' => array('data'=>array('productcard'), 'entity'=>'0'), 	// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context 'all'
 			'moduleforexternal' => 0							// Set this to 1 if feature of module are opened to external users
 		);
 
