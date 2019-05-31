96921e29d15dacf124e7083c199db809a6dc3ebd
atm-florian.m <florian.mortgat@atm-consulting.fr>
2019-05-31 17:42:39 +0200

    predefined entries in the categories dictionary

---
+++ sql/data_llx_c_citrus_category.sql
@@ -0,0 +1,5 @@
+INSERT INTO llx_c_citrus_category (ref, note, active) VALUES
+    ("sour", "Agrume acide", 1),
+    ("bitter", "Agrume amer", 1),
+    ("sweet", "Agrume doux", 1),
+    ("extra-sweet", "Agrume particulièrement sucré", 1);
